3528a76185434d1918a1afe461d197dca3808ec8
one joon <tojbabo@gmail.com>
2026-04-02 22:37:10 +0900

    fix(send): sending data limit 50

--- sender/sender.py
+++ sender/sender.py
@@ -15,20 +15,31 @@ def send2server():
 
     url = "http://localhost:3000/api/windows/set"  # 필요하면 엔드포인트를 바꿔 사용하세요.
 
-    payload = {
-        "id": "identify",          # 클라이언트를 구분할 식별자(필요 시 변경)
-        "count": len(lines),       # data 리스트의 길이
-        "data": list(lines),       # 실제 데이터 리스트
-    }
+    flag = True
 
-    try:
-        response = requests.post(url, json=payload, timeout=5)
-        response.raise_for_status()
-        logger.info(f'sent: {len(lines)} -> status {response.status_code}')
+    while True:
+        if len(lines) == 0: break
+        length = 50 if 50 < len(lines) else len(lines)
 
-        # 서버가 정상 응답했으므로, unsaved 데이터(.tempfile + 메모리)를 모두 제거
-        clearunsaveddata()
-        return True
-    except requests.RequestException as e:
-        logger.error(f'failed to send {len(lines)}: {e}')
-        return False
+        data = lines[:length]
+        lines = lines[length:]
+
+        payload = {
+            "id": "identify",          # 클라이언트를 구분할 식별자(필요 시 변경)
+            "count": len(data),       # data 리스트의 길이
+            "data": list(data),       # 실제 데이터 리스트
+        }
+
+        try:
+            response = requests.post(url, json=payload, timeout=5)
+            response.raise_for_status()
+            logger.info(f'sent: {len(data)} -> status {response.status_code}')
+            
+            # 서버가 정상 응답했으므로, unsaved 데이터(.tempfile + 메모리)를 모두 제거
+        except requests.RequestException as e:
+            logger.error(f'failed to send {len(data)}: {e}')
+            flag = False
+            break
+
+    if flag: clearunsaveddata()
+    return flag
